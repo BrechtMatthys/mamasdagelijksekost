be7a894da9f8402467427cff989f9dc3b9676eef
BrechtMatthys <brecht.matthys@live.be>
2024-06-19 00:05:35 +0200

    Revert "markdown-it"
    This reverts commit d69fb93dd56e4f7b50ed61056171420fb780d261.

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,7 @@
 const { DateTime } = require("luxon");
-const markdownIt = require("markdown-it");
 
 module.exports = function(eleventyConfig) {
-  // Add Passthrough Copies
+
   eleventyConfig.addPassthroughCopy('./src/style.css');
   eleventyConfig.addPassthroughCopy('./src/reset.css');
   eleventyConfig.addPassthroughCopy('./src/assets');
@@ -10,24 +9,16 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('./src/scripts/jquery-3.1.1.js');
   eleventyConfig.addPassthroughCopy('./src/scripts/zoekbalk.js');
 
-  // Add Markdown-it Library
-  let markdownLibrary = markdownIt({
-    html: true,
-    breaks: true,
-    linkify: true
-  });
-  eleventyConfig.setLibrary("md", markdownLibrary);
+  // eleventyConfig.addFilter("postDate", (dateObj) => {
+  //    return DateTime.fromJSDate(dateObj).toLocalString(DateTime.DATE_MED);
+  // })
 
-  // Add Filter (if needed)
-  eleventyConfig.addFilter("postDate", (dateObj) => {
-    return DateTime.fromJSDate(dateObj).toLocalString(DateTime.DATE_MED);
-  });
+  // https://github.com/11ty/eleventy/issues/411 alfabetisch
 
   return {
     dir: {
       input: "src",
       output: "public"
-    },
-    passthroughFileCopy: true // Ensure this is included if it's required
+    }
   };
-};
+}
