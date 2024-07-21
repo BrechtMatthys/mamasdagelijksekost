66732c4a927313d186c46e504bcfc058c7a9f2c0
BrechtMatthys <brecht.matthys@live.be>
2024-07-21 14:51:31 +0200

    Revert "ik revert dit"
    This reverts commit 357e34a0e8cbd09ad897374fd37373e3373489e4.

--- .eleventy.js
+++ .eleventy.js
@@ -1,20 +1,27 @@
+const { DateTime } = require("luxon");
 const markdownIt = require("markdown-it");
-const markdownItOptions = {
-  html: true,
-  breaks: true,
-  linkify: true
-};
 
 module.exports = function(eleventyConfig) {
-  let markdownLibrary = markdownIt(markdownItOptions);
-  eleventyConfig.setLibrary("md", markdownLibrary);
-
+  // Add Passthrough Copies
   eleventyConfig.addPassthroughCopy('./src/style.css');
   eleventyConfig.addPassthroughCopy('./src/reset.css');
   eleventyConfig.addPassthroughCopy('./src/assets');
   eleventyConfig.addPassthroughCopy('./src/admin');
   eleventyConfig.addPassthroughCopy('./src/scripts/jquery-3.1.1.js');
   eleventyConfig.addPassthroughCopy('./src/scripts/zoekbalk.js');
+
+  // Configure Markdown-it Library
+  let markdownLibrary = markdownIt({
+    html: true,
+    breaks: true,
+    linkify: true
+  });
+  eleventyConfig.setLibrary("md", markdownLibrary);
+
+  // Add a Date Filter (if needed)
+  eleventyConfig.addFilter("postDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocalString(DateTime.DATE_MED);
+  });
 
   return {
     dir: {
