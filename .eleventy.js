9f8b0d7ab3ccd3ff1e39632c6ac2ead8c8845174
BrechtMatthys <brecht.matthys@live.be>
2024-07-21 21:17:34 +0200

    IT WORKS
    1 br instead of 2

--- .eleventy.js
+++ .eleventy.js
@@ -5,7 +5,7 @@ module.exports = function(eleventyConfig) {
 
   // Custom filter to convert newlines to <br> tags
   eleventyConfig.addFilter("nl2p", function(text) {
-    return (text || "").replace(/\n/g, "<br>");
+    return (text || "").replace(/(\r\n|\n|\r)/g, "<br>");
   });
 
 
