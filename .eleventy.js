8c49d89f1d3c72f1ec54074d2b484e40ab4558f9
BrechtMatthys <brecht.matthys@live.be>
2024-07-21 14:08:23 +0200

    laatste poging

--- .eleventy.js
+++ .eleventy.js
@@ -10,19 +10,12 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('./src/scripts/jquery-3.1.1.js');
   eleventyConfig.addPassthroughCopy('./src/scripts/zoekbalk.js');
 
-  // eleventyConfig.addFilter("postDate", (dateObj) => {
-  //    return DateTime.fromJSDate(dateObj).toLocalString(DateTime.DATE_MED);
-  // })
-
-  // https://github.com/11ty/eleventy/issues/411 alfabetisch
-
   let markdownLibrary = markdownIt({
     html: true,
     breaks: true,
     linkify: true
   });
   eleventyConfig.setLibrary("md", markdownLibrary);
-
 
   return {
     dir: {
