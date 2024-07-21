869e106e79f6585951686279cf3fb12706a028ed
BrechtMatthys <brecht.matthys@live.be>
2024-07-21 19:52:57 +0200

    weg met die custom widget

--- src/cms/cms.js
+++
@@ -1,5 +0,0 @@
-// src/cms/cms.js
-import CMS from 'netlify-cms';
-import { CustomMarkdownEditorControl, CustomMarkdownEditorPreview } from '../_includes/customMarkdownEditor';
-
-CMS.registerWidget('customMarkdownEditor', CustomMarkdownEditorControl, CustomMarkdownEditorPreview);
