83acf179037b26f73f25f9e256ba537fad2b04e1
BrechtMatthys <brecht.matthys@live.be>
2024-07-30 00:06:13 +0200

    searchbar update?

--- src/scripts/zoekbalk.js
+++ src/scripts/zoekbalk.js
@@ -1,22 +1,21 @@
 // Zoekbalk
 function myFunction() {
   // Declare variables
-  var input, filter, ul, li, a, i, txtValue;
-  input = document.getElementById('myInput');
-  filter = input.value.toUpperCase();
-  ul = document.getElementById("inhoudstafel");
-  li = ul.getElementsByTagName('li');
+  const input = document.getElementById('myInput');
+  const filter = input.value.toUpperCase();
+  const ul = document.getElementById("inhoudstafel");
+  const li = Array.from(ul.getElementsByTagName('li'));
+  
   // Loop through all list items, and hide those who don't match the search query
-  for (i = 0; i < li.length; i++) {
-    a = li[i].getElementsByTagName("a")[0];
-    txtValue = a.textContent || a.innerText;
+  li.forEach(item => {
+    const a = item.getElementsByTagName("a")[0];
+    const txtValue = a.textContent || a.innerText;
     if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      li[i].style.display = "";
+      item.style.display = "";
+    } else {
+      item.style.display = "none";
     }
-    else {
-      li[i].style.display = "none";
-    }
-  }
+  });
 }
 // control f
 $(document).ready(function(){
