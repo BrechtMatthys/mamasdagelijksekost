33b99b21c086d9256bac5dbf1b6bbdac51fac687
BrechtMatthys <brecht.matthys@live.be>
2024-07-31 13:46:15 +0200

    chatgpt search

--- src/scripts/zoekbalk.js
+++ src/scripts/zoekbalk.js
@@ -1,31 +1,33 @@
+// Zoekbalk
 function myFunction() {
   // Declare variables
-  var input = document.getElementById('myInput');
-  var filter = input.value.toUpperCase();
-  var ul = document.getElementById("inhoudstafel");
-  var li = ul.getElementsByTagName('li');
+  var input, filter, ul, li, p, i, txtValue;
+  input = document.getElementById('myInput');
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("inhoudstafel");
+  li = ul.getElementsByTagName('li');
   
-  // Loop through all list items and hide those who don't match the search query
-  for (var i = 0; i < li.length; i++) {
-    var txtValue = li[i].textContent || li[i].innerText;
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < li.length; i++) {
+    p = li[i].getElementsByTagName('p')[0];
+    txtValue = p.textContent || p.innerText;
     if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      // Move the matching element to the beginning of the list
-      ul.insertBefore(li[i], ul.firstChild);
+      li[i].style.display = "";
     } else {
-      // Hide the non-matching element
       li[i].style.display = "none";
     }
   }
 }
 
 // control f
-$(document).ready(function() {
+$(document).ready(function(){
   window.addEventListener("keydown", function(e) {
-    if (e.keyCode === 114 || (e.ctrlKey && e.keyCode === 70)) {
-      const myInput = document.getElementById('myInput');
-      if (!myInput.matches(":focus")) {
+    if (e.keyCode === 114 || (e.ctrlKey && e.keyCode === 70)){
+      if($('#myInput').is(":focus")) {
+        return true;
+      } else {
         e.preventDefault();
-        myInput.focus();
+        $('#myInput').focus();
       }
     }
   });
