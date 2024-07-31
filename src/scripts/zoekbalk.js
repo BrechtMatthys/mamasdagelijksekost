ff84c22a84354020a8d787053a681be89fd99031
BrechtMatthys <brecht.matthys@live.be>
2024-07-31 13:51:03 +0200

    aansluiten

--- src/scripts/zoekbalk.js
+++ src/scripts/zoekbalk.js
@@ -1,20 +1,21 @@
 // Zoekbalk
 function myFunction() {
   // Declare variables
-  var input, filter, ul, li, p, i, txtValue;
+  var input, filter, ul, a, li, p, i, txtValue;
   input = document.getElementById('myInput');
   filter = input.value.toUpperCase();
   ul = document.getElementById("inhoudstafel");
-  li = ul.getElementsByTagName('li');
-  
-  // Loop through all list items, and hide those who don't match the search query
-  for (i = 0; i < li.length; i++) {
-    p = li[i].getElementsByTagName('p')[0];
+  a = ul.getElementsByTagName('a');
+
+  // Loop through all anchor tags, and hide those whose list items don't match the search query
+  for (i = 0; i < a.length; i++) {
+    li = a[i].getElementsByTagName('li')[0];
+    p = li.getElementsByTagName('p')[0];
     txtValue = p.textContent || p.innerText;
     if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      li[i].style.display = "";
+      a[i].style.display = "";
     } else {
-      li[i].style.display = "none";
+      a[i].style.display = "none";
     }
   }
 }
