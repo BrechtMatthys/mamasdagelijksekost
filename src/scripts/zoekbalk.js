ecc291b8dd0eed206c7639b784cf8df6308a1cb6
BrechtMatthys <brecht.matthys@live.be>
2024-07-31 13:07:50 +0200

    search voor html structuur

--- src/scripts/zoekbalk.js
+++ src/scripts/zoekbalk.js
@@ -4,16 +4,15 @@ function myFunction() {
   const input = document.getElementById('myInput');
   const filter = input.value.toUpperCase();
   const ul = document.getElementById("inhoudstafel");
-  const li = ul.getElementsByTagName('a');
+  const li = ul.querySelectorAll('li');
 
   // Loop through all list items and hide those who don't match the search query
-  Array.from(li).forEach((element, index) => {
+  li.forEach((element, index) => {
     const txtValue = element.textContent || element.innerText;
-    const parentElement = element.parentElement;
     if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      parentElement.style.display = "";
+      element.style.display = "";
     } else {
-      parentElement.style.display = "none";
+      element.style.display = "none";
     }
   });
 }
