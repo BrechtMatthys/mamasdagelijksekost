e082ae0ab8082f4e9be8b79b0115b4f8f179f3ff
BrechtMatthys <brecht.matthys@live.be>
2024-07-31 13:01:41 +0200

    Search?

--- src/scripts/zoekbalk.js
+++ src/scripts/zoekbalk.js
@@ -1,36 +1,32 @@
 // Zoekbalk
 function myFunction() {
   // Declare variables
-  var input, filter, ul, li, a, i, txtValue;
-  input = document.getElementById('myInput');
-  filter = input.value.toUpperCase();
-  ul = document.getElementById("inhoudstafel");
-  li = ul.getElementsByTagName('a');
-  // Loop through all list items, and hide those who don't match the search query
-  for (i = 0; i < li.length; i++) {
-    txtValue = li[i].textContent || li[i].innerText;
+  const input = document.getElementById('myInput');
+  const filter = input.value.toUpperCase();
+  const ul = document.getElementById("inhoudstafel");
+  const li = ul.getElementsByTagName('a');
+
+  // Loop through all list items and hide those who don't match the search query
+  Array.from(li).forEach((element, index) => {
+    const txtValue = element.textContent || element.innerText;
+    const parentElement = element.parentElement;
     if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      li[i].parentElement.style.display = "";
+      parentElement.style.display = "";
+    } else {
+      parentElement.style.display = "none";
     }
-    else {
-      li[i].parentElement.style.display = "none";
-    }
-  }
+  });
 }
+
 // control f
-$(document).ready(function(){
-
-  window.addEventListener("keydown",function (e) {
-    if (e.keyCode === 114 || (e.ctrlKey && e.keyCode === 70)){
-        if($('#myInput').is(":focus")) {
-            return true;
-        } else {
-
-            e.preventDefault();
-            $('#myInput').focus();
-        }
+$(document).ready(function() {
+  window.addEventListener("keydown", function(e) {
+    if (e.keyCode === 114 || (e.ctrlKey && e.keyCode === 70)) {
+      const myInput = document.getElementById('myInput');
+      if (!myInput.matches(":focus")) {
+        e.preventDefault();
+        myInput.focus();
+      }
     }
-})
-
-
-})
+  });
+});
